fec00807148fdfcd6ab184ca28a2b2990b6a1895
wangqilun <1217276968@qq.com>
2018-07-17 17:38:55 +0800

    code

--- bayes.py
+++ bayes.py
@@ -13,15 +13,17 @@ def loadDataSet():
     classVec = [0,1,0,1,0,1]
     return postingList, classVec
 
-### 创建词汇表，包含所有文档中出现的不重复的词表
+
 def createVocabList(dataSet):
+    """创建词汇表，包含所有文档中出现的不重复的词表"""
     vocabSet = set([])
     for document in dataSet:
         vocabSet = vocabSet | set(document)
     return list(vocabSet)
 
-### 输出文档向量，向量的每一元素为1或0，分别表示词汇表的单词在输入文档中是否出现
+
 def setOfWords2Vec(vocabList, inputSet):
+    """词集模型   输出文档向量，向量的每一元素为1或0，分别表示词汇表的单词在输入文档中是否出现"""
     returnVec =  [0]*len(vocabList)
     for word in inputSet:
         if word in vocabList:
@@ -30,15 +32,18 @@ def setOfWords2Vec(vocabList, inputSet):
             print "the word： %s is not in my Vocabulary!" % word
     return returnVec
 
+
 def bagOfWords2VecMN(vocabList,inputSet):
+    """词袋模型   输出文档向量，向量的每一元素n，表示词汇表的单词在输入文档中出现的次数"""
     returnVec =  [0]*len(vocabList)
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] += 1
     return returnVec
 
-### 计算概率
+
 def trainNB0(trainMatrix, trainCategory):
+    """计算概率(类条件概率 和 类概率)"""
     numTrainDocs = len(trainMatrix)  # 矩阵行数
     numWords = len(trainMatrix[0])   # 矩阵列数
     pAbusive = sum(trainCategory)/float(numTrainDocs) # 文档属于侮傉性文档的概率
@@ -59,14 +64,17 @@ def trainNB0(trainMatrix, trainCategory):
 
 
 def classifyNB(vec2Clasify, p0Vec, p1Vec, pClass1):
-    p1 = sum(vec2Clasify * p1Vec) + log(pClass1)
+    """朴素贝叶斯分类器"""
+    p1 = sum(vec2Clasify * p1Vec) + log(pClass1)   # p(w|ci)p(ci) 取对数
     p0 = sum(vec2Clasify * p0Vec) + log(1.0 - pClass1)
     if p1 > p0:   # 比较类别的概率，返回大概率对应的类别标签
         return 1
     else:
         return 0
 
+
 def testingNB():
+    """便利函数，该函数封装所有的操作"""
     listOPosts, listClasses = loadDataSet()
     myVocabList = createVocabList(listOPosts)
     trianMat = []
@@ -82,15 +90,18 @@ def testingNB():
 
 
 def textParse(bigString):
+    """文本解析"""
     import re
-    listOfTokens = re.split(r'\w*', bigString)
-    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+    listOfTokens = re.split(r'\w*', bigString)  # 正则划分
+    # 去掉少于两个字符的字符串，并将所有的字符串转换为小写
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2] 
 
 def spamTest():
+    """垃圾邮件测试"""
     docList = []
     classList = []
     fullText = []
-    for i in range(1,26):
+    for i in range(1,26):  # 一共50封邮件
         wordList = textParse(open('email/spam/%d.txt' %i).read())
         docList.append(wordList)
         fullText.extend(wordList)
@@ -102,19 +113,21 @@ def spamTest():
     vocabList = createVocabList(docList)
     trainingSet = range(50)
     testSet = []
-    for i in range(10):
+    for i in range(10):  # 随机选取十封邮件为测试集
         randIndex = int(random.uniform(0,len(trainingSet)))
         testSet.append(trainingSet[randIndex])
-        del(trainingSet[randIndex])
+        #del(trainingSet[randIndex])  # 删除指定值得元素 li = [1,2,4,5]  li.remove(4)  # li = [1,2,5]
+        trainingSet.remove(trainingSet[randIndex])
     trainMat = []
     trainClasses = []
-    for docIndex in trainingSet:
+    for docIndex in trainingSet: 
+        # 训练集中的每封邮件基于词汇表并使用 setOfWords2Vec()函数来构建词向量
         trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
         trainClasses.append(classList[docIndex])
     p0V, p1V, pSpam = trainNB0(array(trainMat),array(trainClasses))
     errorCount = 0
-    for docIndex in testSet:
+    for docIndex in testSet: # 遍历测试集，对其中的每封邮件进行分类
         wordVector = setOfWords2Vec(vocabList, docList[docIndex])
         if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
-            errorCount += 1
+            errorCount += 1   # 如果邮件分类错误，则错误数加1
     print 'the error rate is:', float(errorCount)/len(testSet)
---
+++ logRegres.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from numpy import *
+import math
+
+def loadDataSet():
+    """Logistics回归梯度上升优化算法"""
+    dataMat = []
+    labelMat = []
+    fr = open("testSet.txt")
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
+
+def sigmoid(inX):
+    return 1.0/(1+math.exp(-inX))
+
+def gradAscent(dataMatIn, classLabels):
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    m,n = shape(dataMatrix)
+    alpha = 0.001
+    maxCycles = 500
+    weights = ones((n,1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix * weights)
+        error = (labelMat - h)
+        weights = weights + alpha * dataMatrix.transpose() * error
+    return weights
+
+    def plotBestFit(wei):
+        import matplotlib.pyplot as plt
+        weights = wei.getA()
+        dataMat, labelMat = loadDataSet()
+        dataArr = array(dataMat)
+        n = shape(dataMat)[0]
+        xcord1 = []
+        ycord1 = []
+        xcord2 = []
+        ycord2 = []
+        for i in range(n):
+            if int(labelMat[i]) == 1:
+                xcord1.append(dataArr[i,1])
+                ycord2.append(dataArr[i,2])
+            else:
+                xcord2.append(dataArr[i,1])
+                ycord2.append(dataArr[i,2])
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(xcord1, ycord1, s = 30, c='red', marker='s')
+        ax.scatter(xcord2, ycord2, s = 30, c='greed')
+        x = arange(-3.0, 3.0, 0.1)
+        y = (-weights[0]-weights[1]*x)/weights[2]
+        ax.plot(x,y)
+        plt.xlabel('X1')
+        plt.ylabel('X2')
+        plt.show()
+
+
+def stocGradAscent0(dataMatrix, classLabels):
+    m,n = shape(dataMatrix)
+    alpha = 0.01
+    weights = ones(n)
+    for i in range(m):
+        h = sigmoid(sum(dataMatrix[i]*weights))
+        error = classLabels[i] - h
+        weights = weights + alpha * error  * dataMatrix[i]
+    return weights
+
+def stocGradAscent1(dataMatrix, classLables, numIter = 150):
+    m, n = shape(dataMatrix)
+    weights = ones(n)
+    for j in range(numIter):
+        dataIndex = range(m)
+        for i in range(m):
+            alpha = 4/(1.0+j+i) + 0.01
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex]*weights))
+            error = classLables[randIndex] - h
+            weights = weights + alpha * error  * dataMatrix[randIndex]
+            del(dataIndex[randIndex])
+    return weights
+
+
